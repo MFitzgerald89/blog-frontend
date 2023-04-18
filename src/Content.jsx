8a33dd57017da9afe954f23900a4d933262d619c
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-04-17 22:29:58 -0400

    adds currentpost function

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,22 +6,36 @@ import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
 
-  const handleIndexPosts = () => {
+  const [currentPost, setCurrentPost] = useState({});
+
+  const handleShowPost = (post) => {
+    setIsPostsShowVisible(true);
+    setCurrentPost(post);
+  };
+
+  const handleClose = () => {
+    setIsPostsShowVisible(false);
+  };
+
+  function handleIndexPosts() {
     axios.get("http://localhost:3000/posts.json").then(function (response) {
       console.log(response);
       setPosts(response.data);
     });
-  };
+  }
 
   useEffect(handleIndexPosts, []);
 
   return (
     <div>
       <PostsNew />
-      <PostsIndex posts={posts} />
-      <Modal show={true}>
-        <p>Test</p>
+      <PostsIndex posts={posts} onShowPost={handleShowPost} />
+      <Modal show={isPostsShowVisible} onClose={handleClose}>
+        <h2>Name: {currentPost.title}</h2>
+        <p>Description: {currentPost.description}</p>
+        <p>Price: {currentPost.price}</p>
       </Modal>
     </div>
   );
